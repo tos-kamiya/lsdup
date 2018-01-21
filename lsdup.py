2400442a3992c48b0581d23ab78dc38f6447fedf
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2018-01-21 19:38:00 +0900

    refactor

--- lsdup.py
+++ lsdup.py
@@ -14,25 +14,21 @@ def get_hash(f):
     with open(f, 'rb') as inp:
         return hashlib.md5(inp.read()).hexdigest()
 
-def _check_content(files, show_md5=False):
+def _check_content(files):
     hash2files = defaultdict(list)
     for f in files:
         hash2files[get_hash(f)].append(f)
     for h, fs in hash2files.items():
         if len(fs) >= 2:
-            if show_md5:
-                for f in fs:
-                    print('%s %s' % (h, f))
-            else:
-                print('----\n%s' % '\n'.join(fs))
+            yield h, fs
 
-def find_duplicated_files(files, show_md5=False):
+def find_duplicated_files(files):
     size2files = defaultdict(list)
     for f in files:
         if is_regular_file(f):
             size2files[get_size(f)].append(f)
     for fs in size2files.values():
-        _check_content(fs, show_md5)
+        yield from _check_content(fs)
 
 def main():
     option_show_md5 = False
@@ -41,7 +37,13 @@ def main():
         option_show_md5 = True
         del files[0]
 
-    find_duplicated_files(files, option_show_md5)
+    for h, fs in find_duplicated_files(files):
+        if option_show_md5:
+            for f in fs:
+                print('%s %s' % (h, f))
+        else:
+            print('----\n%s' % '\n'.join(fs))
+
 
 if __name__ == '__main__':
     main()
